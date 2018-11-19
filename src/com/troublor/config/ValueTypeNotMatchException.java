af5bc3be86b13799266a0270c17d136a241b5457
Aaron <troublor@live.com>
2018-11-19 16:15:27 +0800

    add annotations

--- src/com/troublor/config/ValueTypeNotMatchException.java
+++ src/com/troublor/config/ValueTypeNotMatchException.java
@@ -4,32 +4,60 @@ package com.troublor.config;
  * Exception thrown when the type of default value is not the same as that of what is saved in configuration
  */
 public class ValueTypeNotMatchException extends Exception {
+    /**
+     * The class that is input
+     */
     private Class gotClass;
+
+    /**
+     * The class that should be input
+     */
     private Class expectedClass;
 
+    /**
+     * Constructor
+     *
+     * @param gotClass the class that is input
+     * @param expectedClass the class that should be input
+     */
     public ValueTypeNotMatchException(Class gotClass, Class expectedClass) {
         super(
                 String.format(
-                        "Default value type mismatch, expect %s but got %s",
+                        "Value type mismatch, expect %s but got %s",
                         gotClass.toString(), expectedClass.toString())
         );
         this.gotClass = gotClass;
         this.expectedClass = expectedClass;
     }
 
+    /**
+     * Constructor
+     *
+     * @param gotClass the class that is input
+     */
     public ValueTypeNotMatchException(Class gotClass) {
         super(
                 String.format(
-                        "Default value type mismatch, got %s expect unknown",
+                        "Value type mismatch, got %s expect unknown",
                         gotClass.toString())
         );
         this.gotClass = gotClass;
     }
 
+    /**
+     * Getter of gotClass
+     *
+     * @return gotClass
+     */
     public Class getGotClass() {
         return gotClass;
     }
 
+    /**
+     * Getter of ExpectedClass
+     *
+     * @return expectedClass
+     */
     public Class getExpectedClass() {
         return expectedClass;
     }
