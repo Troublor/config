8bc9fc24a64ea709ef431f85187e6d032ed39fa4
Aaron <troublor@live.com>
2018-11-19 16:12:11 +0800

    make it singleton

--- src/com/troublor/config/StringConfig.java
+++ src/com/troublor/config/StringConfig.java
@@ -5,6 +5,20 @@ package com.troublor.config;
  */
 public class StringConfig extends Config<String, String> {
     /**
+     * Singleton instance
+     */
+    private static StringConfig instance = new StringConfig();
+
+    /**
+     * Get singleton instance
+     *
+     * @return singleton instance
+     */
+    public static StringConfig getInstance() {
+        return instance;
+    }
+
+    /**
      * Get configuration with default value
      * if the key does not exist, then return default value provided in parameters
      *
